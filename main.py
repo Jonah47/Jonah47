0426edc487a198950ffdef45676931bcf360c4c2
Jonah <{rikkuben@gmail.com}>
2023-03-19 16:04:49 +0900

    streamlit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
 import time
 
 st.title('Streamlit 超入門')
@@ -19,20 +16,3 @@ for i in range(100):
 
 'Done!!!!'
 
-
-#left_column, right_column = st.columns(2)
-#button = left_column.button('右カラムに文字を表示')
-#if button:
- #   right_column.write('ここは右カラムです')
-
-#expander = st.expander('問い合わせ')
-#expander.write('問い合わせ内容を書く')
-#text = st.text_input('あなたの趣味を教えてください。')
-#condition = st.slider('あなたの今の調子は？', 0, 100, 50,)
-
-#'あなたの趣味:', text
-#'コンディション:', condition, '%'
-
-#if st.checkbox('Show Image'):
-#    img = Image.open('YZF-R1.jpg')
-#    st.image(img, caption='YZF-R1', use_column_width=True)
